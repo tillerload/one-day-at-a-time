0372f48d68e9f72ded6a360f67cbf9049ceca49c
tillerload <tyler.george.work@gmail.com>
2022-11-30 22:45:02 -0330

    intilized firebase and updated readme

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVc-4PsnO-obo5RL-a6PAsu8DM1-V6iOI",
+  authDomain: "one-day-at-a-time-57213.firebaseapp.com",
+  projectId: "one-day-at-a-time-57213",
+  storageBucket: "one-day-at-a-time-57213.appspot.com",
+  messagingSenderId: "47557337505",
+  appId: "1:47557337505:web:1ffd70568b9e3148eaabe0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
